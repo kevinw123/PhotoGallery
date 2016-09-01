fcf411877f9ddfda85ec5153f1d05e50da1fd692
Kevin Wong <kevinwong_895@hotmail.com>
2016-08-31 17:20:48 -0700

    Getting the json data

--- app/src/main/java/com/kevinwong/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/kevinwong/photogallery/PhotoGalleryFragment.java
@@ -5,12 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.io.IOException;
 
 public class PhotoGalleryFragment extends Fragment {
     private static final String TAG = "PhotoGalleryFragment";
@@ -39,12 +36,7 @@ public class PhotoGalleryFragment extends Fragment {
     private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... params){
-            try{
-                String result = new FlickrFetchr().getUrlString("https://www.bignerdranch.com");
-                Log.i(TAG, "Fetched contents of URL: " + result);
-            } catch (IOException ioe){
-                Log.e(TAG, "Failed to fetch URL: ", ioe);
-            }
+            new FlickrFetchr().fetchItems();
             return null;
         }
     }
